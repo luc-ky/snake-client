4278b27e4ffb45910025b008e700f670b7b08b7d
David Luc <luc.ky@me.com>
2023-04-12 20:38:03 -0400

    Add stdin

--- play.js
+++ play.js
@@ -1,9 +1,21 @@
-const net = require("net");
-const {connect} = require("./client")
+const { stdin } = require('process');
+const { connect } = require("./client");
+
+const setupInput = function() {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  return stdin;
+};
+
+const handleUserInput = function(key) {
+  if (key === '\u0003') {
+    process.exit();
+  }
+};
 
 console.log("Connecting ...");
 connect();
-
-module.exports = {
-  connect
-};;
+stdin.on('data', handleUserInput);
+setupInput();
