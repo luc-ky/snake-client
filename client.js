fb66c9cee8be940224180e0ed2a9a96773c5d191
David Luc <luc.ky@me.com>
2023-04-12 20:12:15 -0400

    Modularized the connect function

---
+++ client.js
@@ -0,0 +1,20 @@
+const net = require("net");
+
+const connect = function() {
+  const conn = net.createConnection({
+    host: "165.227.47.243",
+    port: "50541"
+  });
+
+  conn.setEncoding("utf8");
+
+  conn.on("connect", () => {
+    console.log("Connected to server");
+  });
+
+  return conn;
+};
+
+module.exports = {
+  connect
+};;
